07ae3e0001cfc605ce11a28dd3abad5ea68660e9
Hidran Arias <hidran@gmail.com>
2024-01-17 19:40:31 +0100

    Adds bootstrap, navbar, footer and theme-chage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
+import { ThemeTogglerComponent } from './theme-toggler/theme-toggler.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeTogglerComponent,
+    NavbarComponent,
+    FooterComponent
 
 
   ],
---
+++ src/app/theme-toggler/theme-toggler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggler',
+  templateUrl: './theme-toggler.component.html',
+  styleUrl: './theme-toggler.component.css'
+})
+export class ThemeTogglerComponent {
+
+}
